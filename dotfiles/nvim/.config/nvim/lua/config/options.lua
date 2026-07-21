67d7a6e5dcb5731d8761afd00e4e1a1b72c06f30
Clément METZ <metz.clement@gmail.com>
2026-07-21 21:21:05 +0200

    feat(nvim): rewrote vim options. options are now neovim native

---
+++ dotfiles/nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,14 @@
+-- Identation Options
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+
+-- Filetypes
+vim.opt.filetype = "enable"
+
+-- Window Options
+vim.wo.number = true
+vim.wo.colorcolumn = "80"
+vim.wo.cursorline = true
