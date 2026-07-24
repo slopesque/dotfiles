b9669425e22a76ebb368fb5f664740733ac6ffec
Clément METZ <metz.clement@gmail.com>
2026-07-24 17:59:10 +0200

    feat(nvim): yanked text is now dumped in nvim console line

--- dotfiles/nvim/.config/nvim/lua/config/autocommands.lua
+++ dotfiles/nvim/.config/nvim/lua/config/autocommands.lua
@@ -1,3 +1,7 @@
+local strings = require("utils.strings")
+
+DUMP_MAX_LENGTH = 60
+
 -- Autocommand groups
 
 local yank_group = vim.api.nvim_create_augroup("yank", { clear = true })
@@ -9,5 +13,26 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     group = yank_group,
     callback = function()
         vim.hl.on_yank()
+    end,
+})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = "Dump text upon yank",
+    group = yank_group,
+    callback = function()
+        local dumped_text = vim.fn.getreg('"')
+        local bytes_size = #dumped_text
+        local _, lines_count = string.gsub(dumped_text, "\n", "")
+
+        dumped_text = strings.pretty_print_str(dumped_text, DUMP_MAX_LENGTH)
+
+        local output = string.format(
+            "Yanking: `%s` ; %iB %iL",
+            dumped_text,
+            bytes_size,
+            lines_count
+        )
+
+        vim.notify(output)
     end,
 })
